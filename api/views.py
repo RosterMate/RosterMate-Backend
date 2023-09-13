f6eed53e8848e0422ebe19c16052cdf45ff1ddaf
Thejan Weerasekara <thejanbweerasekara@gmail.com>
2023-09-13 12:49:14 +0530

    updated

--- api/views.py
+++ api/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from pymongo import MongoClient
+
+uri = "mongodb+srv://thejanbweerasekara:atYiYnBqom0ZrQXt@rostermatedb.n9yfrig.mongodb.net/"
 
 @api_view(['POST'])
 def user_login(request):
 
     #print("Data from frontend -", request.data['email'], request.data['password'])
 
-    client = MongoClient('localhost', 27017)
-    db = client['rosterMateDB']
+    client = MongoClient(uri)
+    db = client.RosterMateDB
     rmusers_collection = db['RMusers']
     rmusers_data = list(rmusers_collection.find({}))
     client.close()
-
     for user in rmusers_data:
         #print("TEST ",user['email'],request.data['email'],)
         if user['email'] == request.data['email']:
